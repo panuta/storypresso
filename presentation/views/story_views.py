ef0fecacbd67b09c8ab64d842709344fa6e2773d
Panu Tangchalermkul <panuta@gmail.com>
2013-04-11 19:47:55 +0700

    Redesigned read story page, Redesigned view story page

--- presentation/views/story_views.py
+++ presentation/views/story_views.py
@@ -1,11 +1,14 @@
 # -*- encoding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from domain.models import Story
 
 
 def view_story(request, story_uid):
-    return render(request, 'story/story_view.html', {})
+    story = get_object_or_404(Story, uid=story_uid)
+    return render(request, 'story/story_view.html', {'story': story})
 
 
 def read_story(request, story_uid):
-    return render(request, 'story/story_read.html', {})
+    story = get_object_or_404(Story, uid=story_uid)
+    return render(request, 'story/story_read.html', {'story': story})
